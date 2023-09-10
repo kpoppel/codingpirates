4a40d6be6ebde4cf9538fb7aa45a944aae29a9fc
kpoppel <4174578+kpoppel@users.noreply.github.com>
2023-09-10 16:33:44 +0200

    Update dfplayermini.py
    Modulet dokumenteret lidt mere.
    sleep i cmd så kommandoer kan køres efter hinanden.
    query-funktion tilføjet (til filesinfolder og volume)
    play funktion ændret så den vælger et track.
    play_next og play_prev tilføjet til disse funktioner.
    fadeout ændret så den faktisk kan fade både op og ned, og hen over et vilkårligt interval.

--- ESP32/halloween/PythonUdgave/lib/dfplayermini.py
+++ ESP32/halloween/PythonUdgave/lib/dfplayermini.py
@@ -1,75 +1,139 @@
 import utime, time
 from machine import UART, Timer
 
-IDLE = 0
-PAUSED = 1
-PLAYING = 2
-
-
 class Player:
+    MAX_VOLUME = 30
+    
     def __init__(self, pin_TX, pin_RX):
         self.uart = UART(1, 9600, tx=pin_TX, rx=pin_RX)
         self.cmd(0x3F)  # send initialization parametres
         self._fadeout_timer = Timer(-1)
 
-        self._volume = 15
-        self._max_volume = 30
-        self._fadeout_speed = 0
+        # Device resets at volume 30 - we init for 10
+        self._volume = 10
+        self._fade_reset_volume = self._volume
+        self._fade_to_volume = 0
+        self.volume(self._volume)
 
-    def cmd(self, command, parameter=0x00):
-        query = bytes([0x7e, 0xFF, 0x06, command,
-                       0x00, 0x00, parameter, 0xEF])
+    def cmd(self, command, para1=0, para2=0):
+        """
+        The module communication protocol always has this sequence:
+          $S VER Len CMD Feedback para1 para2 sumh suml $O
+          
+          $S = 0x7E
+          VER = version information (0xFF in all the examples)
+          Len = numberof bytes after Len
+          CMD = command byte
+          Feedback = 0x00: no feedback, 0x01: need feedback
+          para1 = query high byte
+          para2 = query low byte
+          sumh,suml = 2 Byte accumulation and verification not including $S
+          $O = 0xEF
+          
+          The device can handle ignoring the checksum and just sending the stop byte.
+          So that is what we do.
+        """
+        query = bytes([0x7E, 0xFF, 0x06, command,
+                       0x00, para1, para2,
+                       0xEF])
         self.uart.write(query)
+        time.sleep_ms(200)
 
-    def _fade_out_process(self, timer):
-        new_volume = self._volume - self._fadeout_speed
-        
-        if new_volume <= 0:
-            print("fadeout finished")
-            new_volume = 0
-            self._fadeout_timer.deinit()
-            self.stop()
-            new_volume = self._max_volume # reset volume to max 
-        self.volume(new_volume)
-
+    def query(self, command, para1=0, para2=0):
+        """
+        Query the module
+        """
+        retry=True
+        while (retry):
+            self.uart.flush()
+            self.cmd(command, para1, para2)
+            time.sleep_ms(200)
+            in_bytes = self.uart.read()
+            if not in_bytes: #timeout
+                return -1
+            if len(in_bytes)==10 and in_bytes[1]==255 and in_bytes[9]==239:
+                retry=False
+        return in_bytes[6]
+    
     # playback
-
-    def play(self, track_id=False):
-        if not track_id:
-            self.resume()
-        elif track_id == 'next':
-            self.cmd(0x01)
-        elif track_id == 'prev':
-            self.cmd(0x02)
+    def play(self, track_id=None):
+        """ Play a track.
+            track_id: If specified, play this track number (1-2999)
+            If not specified playback from last selected. After reset this is 0.
+            Track 0 is the same as track 1.  A track number larger than the number
+            of tracks on the SD card will play the last track.
+        """
+        if track_id is None:
+            self.cmd(0x0D)
         elif isinstance(track_id, int):
-            self.cmd(0x03, track_id)
-
+            self.cmd(0x03, track_id >> 8, track_id & 0xFF)
+    
+    def play_next(self):
+        """ Play next track
+        """
+        self.cmd(0x01)
+        
+    def play_prev(self):
+        """ Play previous track
+        """
+        self.cmd(0x02)
+        
     def pause(self):
+        """ Pause playing
+        """
         self.cmd(0x0E)
 
-    def resume(self):
-        self.cmd(0x0D)
-
     def stop(self):
+        """ Stop playing
+        """
         self.cmd(0x16)
 
-    def fadeout(self, fadeout_ms=1000):
-        # more than 500ms and less than 3000ms
-        fadeout_ms = int(sorted([500, fadeout_ms, 3000])[1])
-        fade_out_step_ms = 100
-        self._fadeout_speed = self._volume * \
-            fade_out_step_ms / fadeout_ms  # ten steps per second
+    def fade(self, fade_ms=3000, to=0, reset=False):
+        """
+        Adjust the volume from the current level to 'to' value over fade_ms.
+        Pause playback when volume reaches 'to' value.
+        If 'reset' is True, volume will be reset to the original value, otherwise
+        stay at the 'to' value.
+        """
+        # Set volume end value
+        self._fade_to_volume = to
+        # Set volume reset value
+        if reset:
+            self._fade_reset_volume = self._volume
+        else:
+            self._fade_reset_volume = to
+
+        # Start timer at intervals
+        fade_ms_per_step = int(fade_ms / self._volume)
         self._fadeout_timer.init(
-            period=fade_out_step_ms, callback=self._fade_out_process)
+            period=fade_ms_per_step,
+            callback=self._fade_process)
+
+    def _fade_process(self, timer):
+        """
+        Timer callback.  Every time it is called we can adjust the volume by 1 step.
+        """
+        if self._volume < self._fade_to_volume:
+            self.volume_up()
+        elif self._volume > self._fade_to_volume:
+            self.volume_down()
+        else:
+            self._fadeout_timer.deinit()
+            # If the current volume and reset volume is not equal reset was True.
+            # In this case we pause the music and reset the volume.  Else just go on.
+            if self._volume != self._fade_reset_volume:
+                self.pause()
+                self.volume(self._fade_reset_volume)
+            #print("fadeout finished")
 
     def loop_track(self, track_id):
-        self.cmd(0x08, track_id)
+        self.cmd(0x08, track_id >> 8, track_id & 0xFF)
 
     def loop(self):
         self.cmd(0x19)
 
     def loop_disable(self):
-        self.cmd(0x19, 0x01)
+        self.cmd(0x19, 0x00, 0x01)
 
     # volume control
 
@@ -81,11 +145,17 @@ class Player:
         self._volume -= 1
         self.cmd(0x05)
 
-    def volume(self, volume=False):
-        if volume:
-            self._volume = int(sorted([0, volume, self._max_volume])[1])
-            print("volume", self._volume)
-            self.cmd(0x06, self._volume)
+    def volume(self, volume=None):
+        """ Set or query volume.
+            volume: None or 0-30
+            If not specified the device is queried for volume setting.
+        """
+        if volume is None:
+            self._volume = self.query(0x43)
+        else:
+            self._volume = int(sorted([0, volume, self.MAX_VOLUME])[1])
+            #print("volume", self._volume)
+            self.cmd(0x06, 0x00, self._volume)
         
         return self._volume
 
@@ -100,17 +170,7 @@ class Player:
     def module_reset(self):
         self.cmd(0x0C)
         
-    # mit
-
+    # file handling
+    
     def filesinfolder(self):
-        retry=True
-        while (retry):
-            self.uart.flush()
-            self.cmd(0x48)
-            time.sleep(0.2)
-            in_bytes = self.uart.read()
-            if not in_bytes: #timeout
-                return -1
-            if len(in_bytes)==10 and in_bytes[1]==255 and in_bytes[9]==239:
-                retry=False
-        return in_bytes[6]
+        return self.query(0x48)
