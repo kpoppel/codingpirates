be4ec057adb3f7fb767782324f262a0a544d125c
esbpo <esbenpouls+github@gmail.com>
2025-04-22 17:10:51 +0200

    Delete luanti/spawn_block v2/on_dig_condition.lua
    Wrong place

--- luanti/spawn_block v2/on_dig_condition.lua
+++
@@ -1,9 +0,0 @@
-on_dig = function (pos, node, digger)
-    -- core.chat_send_all(tostring(progression.flags.wood_flag))
-    if progression.flags.wood_flag then
-        core.chat_send_all("The Idol hums angrily, it will not move.")
-    else
-        core.remove_node(pos)
-        digger:get_inventory():add_item("main","oneblock_v2:idol_wood 1")
-    end
-end,
