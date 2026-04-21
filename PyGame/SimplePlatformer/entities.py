1a20e81759ceeb3af6972b1b997b8027f0607f87
Esben Poulsen <esbenpouls@gmail.com>
2026-04-21 18:22:50 +0200

    Pr Shikuns forespørgsel

--- PyGame/SimplePlatformer/entities.py
+++ PyGame/SimplePlatformer/entities.py
@@ -1,6 +1,7 @@
 # Register entities in this file, coin below as simple example
 import pygame
-from settings import INFO
+from pygame import Vector2 as Vec
+from settings import *
 
 class Coin(pygame.sprite.Sprite):
     def __init__(self, rect:pygame.Rect):
@@ -45,4 +46,48 @@ class Chest(pygame.sprite.Sprite):
         if not self.open:
             self.open = True
             player.coins += 1000
+#---------------------------------------------------
+
+#---------------------------------------------------Enemies
+class SpikedCube(pygame.sprite.Sprite):
+    def __init__(self, rect:pygame.Rect, data:dict):
+        super().__init__()
+        self.rect = rect
+        self.image = pygame.image.load(INFO.assetsPath+"spiked_cube_enemy.png")
+        self.image = pygame.transform.scale(self.image, (self.rect.width, self.rect.height))
+        self.startPoint = Vec(data["x"]/TILE_SIZE, data["Y"]/TILE_SIZE)
+        self.endPoint = Vec(data["customFields"]["EndLocation"]["cx"], data["customFields"]["EndLocation"]["cy"])
+        self.speed = data["customFields"]["speed"]
+        self.direction = "toEnd"
+
+    def move(self):
+        diff = Vec()
+        location = Vec(self.rect.x, self.rect.y)
+        new_location = Vec()
+
+        if self.direction == "toEnd":
+            diff.x = self.endPoint.x - self.rect.x
+            diff.y = self.endPoint.y - self.rect.y
+        elif self.direction == "toStart":
+            diff.x = self.endPoint.x - self.rect.x
+            diff.y = self.endPoint.y - self.rect.x
+        
+        unitVector = diff/diff.length
+        movement = unitVector * self.speed
+
+        if self.direction == "toEnd":
+            if movement.length > diff.length:
+                new_location = self.endPoint
+            else:
+                new_location = location + movement
+                
+        elif self.direction == "toStart":
+            if movement.length > diff.length:
+                new_location = self.startPoint
+            else:
+                new_location = location + movement
+            
+        self.rect.x = new_location.x
+
+
 #---------------------------------------------------
